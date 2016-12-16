4b6ef4461e9de4bc15a03bc8d6360371763f5fce
Vijay Punna <vijaypunna@aol.com>
2016-12-15 19:40:20 -0600

    My Resume Upload v1

---
+++ resume.js
@@ -0,0 +1,90 @@
+$(function(){
+	
+	//pagination
+	paging("jyo-exp", "hdn-exp");
+	paging("jyo-proj", "hdn-proj");
+	
+	function paging(className, hiddenValue){
+		var pages = 0;
+		$("." + className).each(function(){
+			if($(".jyo-exp").index($(this)) != 0){
+				$(this).hide();
+			}//if
+			pages++;
+		});//each
+		
+		$(".jyo-rgt-btn").click(function(){
+			var current = parseInt($("#" + hiddenValue).val());
+			if(current != pages - 1){
+				$("." + className).each(function(){
+					if($("." + className).index($(this)) == current+1) $(this).show();
+					else $(this).hide();
+				});//each
+				$("#" + hiddenValue).val(current + 1);
+			}
+
+		});//click
+		
+		$(".jyo-lft-btn").click(function(){
+			var current = parseInt($("#" + hiddenValue).val());
+			if(current > 0){
+				$("." + className).each(function(){
+					if($("." + className).index($(this)) == current -1)$(this).show();
+					else $(this).hide();
+				});//each
+				$("#" + hiddenValue).val(current - 1);
+			}
+		});//click
+	}
+	
+	//
+	$(".jyo-blacken").css("color", "#cc9900");
+	//nav
+	$( ".jyo-nav" ).fadeOut(0.000000001);
+	
+	$(".jyo-nav-button").click(function(){
+		$(".jyo-nav").fadeIn(2000);
+		$(".jyo-blacken").css("color", "#664d00");
+		$(".jyo-main-hdng").css("color", "#664d00");
+		$(".jyo-sub-hdng").css("color", "#664d00");
+	});//click
+	
+	$(".jyo-white").click(function(){
+		$(".jyo-blacken").fadeOut(0.000000001);
+		$(".jyo-blacken").css("color", "#cc9900");
+		$(".jyo-main-hdng").css("color", "#fff");
+		$(".jyo-sub-hdng").css("color", "#02b3ff");
+		$(".jyo-blacken").fadeIn(2000);
+		$( ".jyo-nav" ).fadeOut(0.000000001);
+	});//click
+		//show hide
+			var content = {"experienceLink" : "experience", "skillsLink" : "skills", "projectsLink" : "projects", "aboutLink" : "about"};
+
+			showHide(content, "experienceLink");
+			$.each(content, function(key, value){
+				$("#"+key).click(function(){
+					showHide(content, key);
+				});//click
+			});
+
+			var skills = {"frontEndLink" : "frontEnd", "backEndLink" : "backEnd", "toolsLink" : "tools", "otherLink" : "others"};
+
+			showHide(skills, "frontEndLink");
+			$.each(skills, function(key, value){
+				$("#"+key).click(function(){
+					showHide(skills, key);
+				});//click
+			});
+			
+			$("#projectsLink").click(function(){
+				$("#jyo-proj-1").show();
+			});
+			
+		});
+
+		function showHide(list, item){
+			$.each(list, function(key, value){
+				if(key == item) $("#"+value).show();
+				else $("#"+value).hide();
+			});//each function
+		}//showHide
